dd986346bfc392971c1804de84d11a3b0ec0c987
Konstantin Polyakov <shyzik_93@mail.ru>
2018-05-22 00:16:42 +0300

    mysql_escape_string was replaced

--- lib.class.teaser.php
+++ lib.class.teaser.php
@@ -21,12 +21,12 @@ class Teaser extends Addon {
         $this->tbl_teaser_type_parent = "`".TABLE_PREFIX."mod_wbs_teasers_type_parent`";
         $this->tbl_teaser_type_any_urls = "`".TABLE_PREFIX."mod_wbs_teasers_type_any_urls`";
         $this->tbl_teaser_type_dir = "`".TABLE_PREFIX."mod_wbs_teasers_type_dir`";
-	}
+        }
     
     public function add_teaser($page_id, $section_id) {
-        $before_tile = mysql_escape_string("<div style='text-align:center;'>");
-        $after_tile = mysql_escape_string("</div>");
-        $tile = mysql_escape_string("<div class='tizerbox'>
+        $before_tile = $this->db->escapeString("<div style='text-align:center;'>");
+        $after_tile = $this->db->escapeString("</div>");
+        $tile = $this->db->escapeString("<div class='tizerbox'>
                  <a href='{{page_url}}'><img src='{{wb_url}}/media/teaser/{{page_id}}.png'></a>
                  <br>
                  <a href='{{page_url}}'>{{page_title}}</a>
@@ -47,48 +47,48 @@ class Teaser extends Addon {
         $sql = "INSERT INTO {$this->tbl_teaser_type_dir}
                 SET `page_id`=$page_id, `section_id`=$section_id";
         $this->db->query($sql);
-	}
+        }
     
     public function delete_teaser($section_id) {
         $this->db->query("DELETE FROM {$this->tbl_teaser} WHERE section_id = '$section_id'");
         $this->db->query("DELETE FROM {$this->tbl_teaser_type_parent} WHERE section_id = '$section_id'");
         $this->db->query("DELETE FROM {$this->tbl_teaser_type_dir} WHERE section_id = '$section_id'");
-	}
+        }
     
     //public function update_teaser($section_id, $tile, $type, $before_tile, $after_tile, $is_active) {
     public function update_teaser($section_id, $fields) {
-        //$tile = mysql_escape_string($tile);
-        //$type = mysql_escape_string($type);
-        //$before_tile = mysql_escape_string($before_tile);
-        //$after_tile = mysql_escape_string($after_tile);
-        //$is_active = mysql_escape_string($is_active);
+        //$tile = $this->db->escapeString($tile);
+        //$type = $this->db->escapeString($type);
+        //$before_tile = $this->db->escapeString($before_tile);
+        //$after_tile = $this->db->escapeString($after_tile);
+        //$is_active = $this->db->escapeString($is_active);
         //$sql = "UPDATE {$this->tbl_teaser} SET `tile`='$tile', `type`='$type', `before_tile`='$before_tile', `after_tile`='$after_tile', `is_active`='$is_active' WHERE `section_id`='$section_id'";
         $sql = build_update($this->tbl_teaser, $fields, glue_fields(['section_id'=>$section_id], 'AND'));
         return $this->db->query($sql);
     }
 
     public function update_type_parent_page($section_id, $parent_id, $except_child_ids) {
-        $parent_id = mysql_escape_string($parent_id);
-        $except_child_ids = mysql_escape_string($except_child_ids);
+        $parent_id = $this->db->escapeString($parent_id);
+        $except_child_ids = $this->db->escapeString($except_child_ids);
         $sql = "UPDATE {$this->tbl_teaser_type_parent} SET `parent_id`='$parent_id', `except_child_ids`='$except_child_ids' WHERE `section_id`='$section_id'";
         return $this->db->query($sql);
     }
 
     public function update_type_dir($section_id, $dir) {
-        $dir = mysql_escape_string($dir);
+        $dir = $this->db->escapeString($dir);
         $sql = "UPDATE {$this->tbl_teaser_type_dir} SET `dir`='$dir' WHERE `section_id`='$section_id'";
         return $this->db->query($sql);
     }
-	
+        
     public function update_type_any_urls($section_id, $any_urls_id, $protocol, $url, $pic_dir, $title) {
         $errs = '';
         $i = 0;
         while($i < count($any_urls_id)) {
-            $any_urls_id[$i] = mysql_escape_string($any_urls_id[$i]);
-            $protocol[$i] = mysql_escape_string($protocol[$i]);
-            $url[$i] = mysql_escape_string($url[$i]);
-            $pic_dir[$i] = mysql_escape_string($pic_dir[$i]);
-            $title[$i] = mysql_escape_string($title[$i]);
+            $any_urls_id[$i] = $this->db->escapeString($any_urls_id[$i]);
+            $protocol[$i] = $this->db->escapeString($protocol[$i]);
+            $url[$i] = $this->db->escapeString($url[$i]);
+            $pic_dir[$i] = $this->db->escapeString($pic_dir[$i]);
+            $title[$i] = $this->db->escapeString($title[$i]);
 
             $sql = "UPDATE {$this->tbl_teaser_type_any_urls} SET `protocol`='{$protocol[$i]}', `url`='{$url[$i]}', `pic_dir`='{$pic_dir[$i]}',  `title`='{$title[$i]}' WHERE `section_id`='{$section_id}' AND `any_urls_id`='{$any_urls_id[$i]}'";
             if (!$this->db->query($sql)) $errs .= '<br><br>\n\n'.$database->get_error();
@@ -98,9 +98,9 @@ class Teaser extends Addon {
     }
     
     public function add_type_any_urls($page_id, $section_id, $duplicate_count, $protocol, $url, $pic_dir) {
-        $protocol = mysql_escape_string($protocol);
-        $url = mysql_escape_string($url);
-        $pic_dir = mysql_escape_string($pic_dir);
+        $protocol = $this->db->escapeString($protocol);
+        $url = $this->db->escapeString($url);
+        $pic_dir = $this->db->escapeString($pic_dir);
 
         $sql = "INSERT INTO 
                    {$this->tbl_teaser_type_any_urls} (`page_id`, `section_id`, `protocol`, `url`, `pic_dir`)
