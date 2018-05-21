c6d179da9d52bd2328431b62bda6eda81bebdf68
Konstantin Polyakov <shyzik_93@mail.ru>
2018-05-22 00:44:08 +0300

    wbs prefix was added

--- lib.functions.php
+++ lib.functions.php
@@ -12,7 +12,7 @@
 function get_teaser($section_id) {
     global $database;
 
-    $sql = "SELECT * FROM `".TABLE_PREFIX."mod_teasers` WHERE `section_id`=".(int)$section_id;
+    $sql = "SELECT * FROM `".TABLE_PREFIX."mod_wbs_teasers` WHERE `section_id`=".(int)$section_id;
     $row = $database->query($sql);
     if ($database->is_error()) {echo $database->get_error();}
     $row = $row->fetchRow();
@@ -20,7 +20,7 @@ function get_teaser($section_id) {
     $mark = $row['common_settings_mark'];
     $type = $row['type'];
     $is_active = $row['is_active'];
-	if ($row['common_settings_mark'] !== null && $row['common_settings_mark'] !== '') {
+        if ($row['common_settings_mark'] !== null && $row['common_settings_mark'] !== '') {
         $sql = "SELECT * FROM `".TABLE_PREFIX."mod_teasers` WHERE `section_id`='{$mark}'";
         $row = $database->query($sql);
         if ($database->is_error()) {echo $database->get_error();}
@@ -28,14 +28,14 @@ function get_teaser($section_id) {
         $row['common_settings_mark'] = $mark;
         $row['type'] = $type;
         $row['is_active'] = $is_active;
-	}
+        }
     
     return $row;
 }
 
 function get_teaser_type_parent_page($section_id) {
     global $database;
-    $sql = 'SELECT * FROM `'.TABLE_PREFIX.'mod_teasers_type_parent` WHERE `section_id`='.(int)$section_id;
+    $sql = 'SELECT * FROM `'.TABLE_PREFIX.'mod_wbs_teasers_type_parent` WHERE `section_id`='.(int)$section_id;
     $r = $database->query($sql);
     if ($database->is_error()) echo $database->get_error();
     $r = $r->fetchRow();
@@ -44,7 +44,7 @@ function get_teaser_type_parent_page($section_id) {
 
 function get_teaser_type_dir($section_id) {
     global $database;
-    $sql = 'SELECT * FROM `'.TABLE_PREFIX.'mod_teasers_type_dir` WHERE `section_id`='.(int)$section_id;
+    $sql = 'SELECT * FROM `'.TABLE_PREFIX.'mod_wbs_teasers_type_dir` WHERE `section_id`='.(int)$section_id;
     $r = $database->query($sql);
     if ($database->is_error()) echo $database->get_error();
     $r = $r->fetchRow();
@@ -53,18 +53,18 @@ function get_teaser_type_dir($section_id) {
 
 function get_teaser_type_any_urls($section_id) {
     global $database;
-    $sql = 'SELECT * FROM `'.TABLE_PREFIX.'mod_teasers_type_any_urls` WHERE `section_id`='.(int)$section_id;
+    $sql = 'SELECT * FROM `'.TABLE_PREFIX.'mod_wbs_teasers_type_any_urls` WHERE `section_id`='.(int)$section_id;
     $r = $database->query($sql);
     if ($database->is_error()) echo $database->get_error();
     return $r;
 }
 
 /*function replace_vars($str, $vars) {
-	$_vars = [];
-	foreach ($vars as $name => $value) {
-		$_vars['{{'.strtoupper($name).'}}'] = $value;
-	}
-	return str_replace(array_keys($_vars), array_values($_vars), $vars);
+        $_vars = [];
+        foreach ($vars as $name => $value) {
+                $_vars['{{'.strtoupper($name).'}}'] = $value;
+        }
+        return str_replace(array_keys($_vars), array_values($_vars), $vars);
 }
 function process_pic_dir($pic_dir, $prefix=WB_URL) {
     replace_vars($pic_dir, $vars);
